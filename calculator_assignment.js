3aa057d3720a21fcf1a6276108e9a20e2d4dd4f9
unknown <omilude.omololamercy@gmail.com>
2022-11-27 23:45:18 +0100

    calculator

---
+++ calculator_assignment.js
@@ -0,0 +1,20 @@
+let answer=document.getElementById("output");
+
+let calculate=(number)=>{
+    answer.value+=number;
+}
+
+ 
+ let Result=()=>{
+
+ }
+ 
+
+ 
+function clr(){
+    answer.value=""
+}
+ 
+ function del(){
+    answer.value=answer.value.slice(0,-1)
+ }
